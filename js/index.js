0c42ef27cb0ee67936d1a655fd5cc2225f3d851f
LoboArkano <pipiolo_mananes@hotmail.com>
2021-10-07 01:02:24 -0500

    Add functions to show and hide mobile menu

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+const showM = () => {
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  mobileMenu.classList.remove('hidden');
+};
+
+const hideM = () => {
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  mobileMenu.classList.add('hidden');
+};
